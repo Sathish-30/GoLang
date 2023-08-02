f701ffecac9abc0533f4d8a844809726ae5736db
Sathish-39 <sathish.amsaravanan@gmail.com>
2023-08-02 21:37:34 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+// Go progarm are managed by the packages
+// A package is a collection of source file
+import "fmt"
+
+// It requires a entry point , mean a main function
+func main(){
+	fmt.Println("Welcome to our conference booking application")
+	fmt.Println("Get your ticktes here to attend")
+}
